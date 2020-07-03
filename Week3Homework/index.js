876627a5a0a4f16a6be46f336151263a79a7e2ab
Asma Charef <asma.charef@hotmail.com>
2020-07-02 19:26:50 -0500

    update2

---
+++ Week3Homework/index.js
@@ -0,0 +1,9 @@
+const arr =["onion", "chicken", "olive", "spinach" ];
+function greetCustomer() {
+    ;
+    for (topp of arr){
+    console.log("Welcome to Pizza House, our toppings are "+topp);
+}
+
+
+}
