6ee1db747bf547d2b8df9579ef7d33b2fcaa1f24
Asma Charef <asma.charef@hotmail.com>
2020-07-26 14:01:18 -0500

    modify

--- Week6Homework/index.js
+++ Week6Homework/index.js
@@ -5,7 +5,10 @@ class Product {
     this.brand = brand;
 }
 }
-class TShirt extends Product {
+ let product1 = new Product(1, "shirt", "nike");
+ let product2 = new Product(2, "shoes", "nike");
+
+class Shirt extends Product {
     constructor(id, category, brand, description, price, size, color, type){
     super(id, category, brand);
     this.name = name;
@@ -13,8 +16,11 @@ class TShirt extends Product {
     this.price = price;
     this.size  = size;
     this.color = color;
+    this.type = type;
 }
 }
+let TShirt = new Shirt(1, "shirt", "nike","short Sleeve tshirt","$50",["s","m","l"],["red","blue","black"],"clothes");
+
 class Shoes extends Product {
     constructor(id, category, brand, name, description, price, size, color, type, typeId){
     super(id, category, brand);
@@ -27,3 +33,7 @@ class Shoes extends Product {
     this.typeId = typeId;
 }
 }
+
+let vega = new Shoes(2, "shoes", "nike", "high top running shoes","$60",[7,8,9,10],["black","red"],"running","lightweight");
+
+console(vega.brand);
